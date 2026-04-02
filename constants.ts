82266b570a687a4fe5023e43e703c6fd52bad9e3
plnesbukittinggi-ai <plnes.bukittinggi@gmail.com>
2026-04-02 12:51:39 +0700

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,7 +1,7 @@
 
 import { ULPName, ULPData } from './types';
 
-export const APP_VERSION = '1.0.4';
+export const APP_VERSION = '1.0.5';
 
 export const DATA_ULP: Record<string, ULPData> = {
   [ULPName.BUKITTINGGI]: {
